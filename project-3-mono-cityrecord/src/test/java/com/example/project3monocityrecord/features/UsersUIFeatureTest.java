269b8cbcfdc1871686000e4820b54f9be173d7e6
austino1 <32141431+austino1@users.noreply.github.com>
2018-04-02 23:58:15 -0700

    first test passing in UI feature test

--- project-3-mono-cityrecord/src/test/java/com/example/project3monocityrecord/features/UsersUIFeatureTest.java
+++ project-3-mono-cityrecord/src/test/java/com/example/project3monocityrecord/features/UsersUIFeatureTest.java
@@ -1,5 +1,6 @@
 package com.example.project3monocityrecord.features;
 
+import com.example.project3monocityrecord.models.User;
 import com.example.project3monocityrecord.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Before;
@@ -8,6 +9,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -26,4 +32,27 @@ public class UsersUIFeatureTest {
         userRepository.deleteAll();
     }
 
+    @Test
+    public void shouldAllowFullCrudFunctionalityForAUser() throws Exception {
+        User firstUser = new User(
+                "FeatName",
+                "FeatPass"
+        );
+
+        firstUser = userRepository.save(firstUser);
+        Long firstUserId = firstUser.getId();
+
+        User secondUser = new User(
+                "SecName",
+                "SecPass"
+        );
+
+        secondUser = userRepository.save(secondUser);
+        Long secondUserId = secondUser.getId();
+
+        System.setProperty("selenide.browser", "Chrome");
+        open("http://localhost:3000");
+        $$("[data-user-display]").shouldHaveSize(2);
+
+    }
 }
